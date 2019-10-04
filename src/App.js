1af51773cc67531b021600249495c91540ec0e30
Nothingis <thenothinis@gmail.com>
2019-10-04 19:43:24 +0900

    Updates

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SpeedQuiz from './components/SpeedQuiz';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <SpeedQuiz>Quiz...</SpeedQuiz>
+    );
+  }
+}
+
+export default App;
